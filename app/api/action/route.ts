eedd9fd8fd34bffdcfa952ea16e1b96254935cb2
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-09-02 14:19:08 +0100

    test

--- app/api/action/route.ts
+++ app/api/action/route.ts
@@ -2,12 +2,7 @@ import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-    const body: FrameRequest = await req.json();
-    const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
-    
-    if (!isValid) {
-      return new NextResponse('Message not valid', { status: 500 });
-    }
+
     return NextResponse.json({ message: 'Hello from the frame route' }, { status: 200 });
   }
   
